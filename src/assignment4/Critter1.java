1530168f86b4ba1fd16fe0de1557aaa80f9387df
ehp355 <enrique_perez@utexas.edu>
2016-10-19 20:14:11 -0500

    Adde javadoc comments to top of critter1 and critter2

--- src/assignment4/Critter1.java
+++ src/assignment4/Critter1.java
@@ -1,23 +1,29 @@
 package assignment4;
 /**
- * 
+ *
  * @author Enrique
- *Skeleton for Critter1, needs work [10/17/16]
- */
+ *Critter1 starts with a random direction, when it walks, it will walk back and forth
+ *from its original position and an adjacent position. Craig is the natural predator to
+ *Critter1 so when fight is called it will choose not to fight craig but instead will reproduce
+ *a child in an adjacent space that is perpendicular to the parent's movement. Critter1 is the
+ *natural predator Critter2 so it will automatically return true when it is set to fight Critter2.
+ *With any other versions of Critter its random decision for Critter1. The runStats methods
+ *will print out how many children each living Critter1 has reproduced.
+*/
 public class Critter1 extends Critter {
 	private int dir;
 	private int children;
-	
+
 	@Override
 	public String toString(){return "1";}
-	
-	
+
+
 	public Critter1(){
 		dir = Critter.getRandomInt(8);
 	}
-	
+
 	/**
-	 * Craig is the natural predator to Critter1, so Critter1 will run away if 
+	 * Craig is the natural predator to Critter1, so Critter1 will run away if
 	 * return false if faced against craig.Critter1 is the natural predator to
 	 * Critter2, so it will always fight if faced against Critter2. Anything else
 	 * it will be based on a random roll. 1 says it fights 0 says it runs
@@ -39,18 +45,18 @@ public class Critter1 extends Critter {
 			int rand = Critter.getRandomInt(1);
 			return rand==1;
 		}
-		
+
 
 	}
-	
+
 	@Override
 	public void doTimeStep(){
 		CalculateWalk();
 		walk(dir);
 	}
-	
+
 	/**
-	 * method to force Critter1 to walk back and forth, its children will back and forth but 
+	 * method to force Critter1 to walk back and forth, its children will back and forth but
 	 * in the direction 90degrees to its parents direction
 	 */
 	public void CalculateWalk(){
@@ -60,13 +66,13 @@ public class Critter1 extends Critter {
 			dir = dir -4;
 		}
 	}
-	
+
 	/**
 	 * prints out how manys kids each Critter1 on the board has had.
 	 * @param critter1
 	 */
 	public static void runStats(java.util.List<Critter> critter1){
-		
+
 		for(Object obj : critter1){
 			Critter1 c = (Critter1) obj;
 			System.out.println("This Critter1 had "+ c.children+" kids.");
